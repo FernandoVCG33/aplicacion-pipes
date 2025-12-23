a3a2f42d5b005c948983582c05367530baaffbf4
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-23 11:34:41 -0400

    paginas agregadas y signasl

--- pipe-app/src/app/pages/basic-page/basic-page.ts
+++ pipe-app/src/app/pages/basic-page/basic-page.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {LowerCasePipe, TitleCasePipe, UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [
+    LowerCasePipe,
+    UpperCasePipe,
+    TitleCasePipe
+  ],
   templateUrl: './basic-page.html',
 })
 export default class BasicPage {
-
+  nameLower= signal('fernando');
+  nameUpper=signal('FERNANDO');
+  fullName=signal('fErnanDo caTacOra');
 }
--- pipe-app/src/app/pages/numbers-page/numbers-page.ts
+++ pipe-app/src/app/pages/numbers-page/numbers-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './numbers-page.html',
 })
 export default class NumbersPage {
+    totalSell=signal(2_423_232.5567);
+    percet=signal(0.5846);
 
 }
