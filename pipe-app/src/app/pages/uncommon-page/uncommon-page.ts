98f2b14aabdc2adc2562758799399fa8a6551c60
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-23 17:11:09 -0400

    mejorasy mapeo pero falta y mas

--- pipe-app/src/app/pages/uncommon-page/uncommon-page.ts
+++ pipe-app/src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,24 +1,25 @@
 import {Component, signal} from '@angular/core';
 import {Card} from '../../components/card/card';
-import {I18nSelectPipe} from '@angular/common';
+import {I18nPluralPipe, I18nSelectPipe, JsonPipe, KeyValuePipe, SlicePipe, UpperCasePipe} from '@angular/common';
 
 const client1={
   name:'Fernando',
-  gender:'Male',
+  gender:'male',
   age:21,
   address:'Ottawa, Canada'
 }
 
 const client2={
   name:'Cindy',
-  gender:'Female',
+  gender:'female',
   age:20,
   address:'Toronto, Canada'
 }
+
 @Component({
   selector: 'app-uncommon-page',
   imports: [
-    Card,I18nSelectPipe
+    Card, I18nSelectPipe, I18nPluralPipe, SlicePipe, JsonPipe, UpperCasePipe,KeyValuePipe
   ],
   templateUrl: './uncommon-page.html',
 })
@@ -35,5 +36,36 @@ export default class UncommonPage {
       return;
     }
     this.client.set(client1);
+  }
+
+  clientsMap=signal({
+    '=0':'no tenemos ningún cliente esperando.',
+    '=1':'tenemos un cliente esperando.',
+    '=2':'tenemos 2 clientes esperando.',
+    'other':'tenemos # clientes esperando.'
+  })
+  clients=signal([
+    'Maria',
+    'Pedro',
+    'Juan',
+    'Ana',
+    'Luis',
+    'Laura',
+    'Carlos',
+    'Marta',
+    'Jose',
+    'Lucia'
+  ]);
+  deleteClient()
+  {
+    this.clients.update(prev => prev.slice(1));
+  }
+
+  //keyValue Pipe
+  person={
+    name:'Fernando',
+    age:21,
+    address:'Ottawa, Canada',
+    profession:'Software Developer'
   }
 }
