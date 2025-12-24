78db56ed4164126c854d47a17ae62595a2a32a0f
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-24 13:30:52 -0400

    inicio de los filtros

--- pipe-app/src/app/pages/custom-page/custom-page.ts
+++ pipe-app/src/app/pages/custom-page/custom-page.ts
@@ -2,12 +2,16 @@ import {Component, signal} from '@angular/core';
 import {ToggleCasePipe} from '../../pipes/toggle-case.pipe';
 import {heroes} from '../../data/heroes.data';
 import {NamePipe} from '../../interfaces/color-hero.pipe';
+import {HeroCreatorPipe} from '../../pipes/hero-creator.pipe';
+import {UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-custom-page',
   imports: [
     ToggleCasePipe,
-    NamePipe
+    NamePipe,
+    HeroCreatorPipe,
+    UpperCasePipe
   ],
   templateUrl: './custom-page.html',
 })
